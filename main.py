23f2ff9035c74e080cfc473bf5cebd0bc7fd67f5
aneesh-sathe <117112887+aneesh-sathe@users.noreply.github.com>
2024-01-21 00:30:18 +0530

    app.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torchvision
-
-def main():
-    print('torch version :', torch.__version__)
-    print('torchvision version:', torchvision.__version__)
-
-if __name__ == "__main__":
-    main()
