c6a106778aed6ea565e5f099c096cc5b8ba27c62
aneesh-sathe <117112887+aneesh-sathe@users.noreply.github.com>
2024-01-21 00:19:36 +0530

    run inference

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import torch
+import torchvision
 
 def main():
-    print('hello world')
+    print('torch version :', torch.__version__)
+    print('torchvision version:', torchvision.__version__)
 
 if __name__ == "__main__":
     main()
