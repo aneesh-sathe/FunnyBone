bd199802b01638d9694b73e8b49283f743b66557
aneesh-sathe <117112887+aneesh-sathe@users.noreply.github.com>
2024-02-22 12:57:05 +0530

    downloadable report

--- generate.py
+++ generate.py
@@ -7,7 +7,7 @@ output_parser = StrOutputParser()
 
 chat_template = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a helpful assistant of the Best Orthopaedic Surgeon in the world. You are responsible for writing detailed medical reports which include the following sections <FRACTURE DETECTED> <TREATMENT PLAN> <COST BREAKUP> <ADDITIONAL RECOMMENDATIONS>. If no fracture is detected, you should forward the case to the doctor for special attention."),
+        ("system", "You are a helpful assistant of the Best Orthopaedic Surgeon in the world. You are responsible for writing detailed medical reports which include the following sections <TYPE OF FRACTURE> <TREATMENT PLAN> <COST BREAKUP> <ADDITIONAL RECOMMENDATIONS>. Assume any other details like Patient Name, Total Cost etc whenever necessary."),
         ("human", "Hello! I have been experiencing some pain in {fracture_type}"),
         ("ai", "Oh, Let me take a so that I can make a report. How certain are you of the pain?"),
         ("human", "I'm {confidence}% certain. Please make a report. " )
@@ -18,9 +18,10 @@ chat_template = ChatPromptTemplate.from_messages(
 chain = chat_template | llm | output_parser
 
 def make_report(result):
+    
     report = chain.invoke({
-        "fracture_type": result.condition,
-        "confidence": result.confidence
+        "fracture_type": result.get("condition"),
+        "confidence": result.get("confidence")
     })
     
     return report
